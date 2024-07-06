9aad3f92313796ac8100416ce64cb5ea2893a7cb
Puneeth kumar vh <69886724+puneethkumarvh@users.noreply.github.com>
2024-07-06 14:12:19 +0530

    Create MergeSort.java

---
+++ MergeSort.java
@@ -0,0 +1,93 @@
+public class MergeSort {
+    
+    // Main function that sorts array[left...right] using merge()
+    public void sort(int[] array, int left, int right) {
+        if (left < right) {
+            // Find the middle point
+            int middle = (left + right) / 2;
+
+            // Sort first and second halves
+            sort(array, left, middle);
+            sort(array, middle + 1, right);
+
+            // Merge the sorted halves
+            merge(array, left, middle, right);
+        }
+    }
+
+    // Merges two subarrays of array[]
+    // First subarray is array[left...middle]
+    // Second subarray is array[middle+1...right]
+    private void merge(int[] array, int left, int middle, int right) {
+        // Find sizes of two subarrays to be merged
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        // Create temporary arrays
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copy data to temporary arrays
+        for (int i = 0; i < n1; ++i) {
+            leftArray[i] = array[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            rightArray[j] = array[middle + 1 + j];
+        }
+
+        // Merge the temporary arrays
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of leftArray[] if any
+        while (i < n1) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of rightArray[] if any
+        while (j < n2) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    // A utility function to print array of size n
+    public static void printArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Given Array");
+        printArray(array);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(array, 0, array.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(array);
+    }
+}
